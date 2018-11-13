4fd72fb95add7aa9edfe4ed21b5c96bdd98612d7
talrez <tal@angelsense.com>
2018-11-13 22:09:26 +0200

    Added test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { PersonCompComponent } from './person-comp/person-comp.component';
 import { PersonComp2Component } from './person-comp2/person-comp2.component';
 import { DirectivesComponent } from './directives/directives.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonCompComponent,
     PersonComp2Component,
-    DirectivesComponent
+    DirectivesComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
